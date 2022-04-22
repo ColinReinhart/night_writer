ca2c2e89f10315fc0fe9321ae295a9b5fa2f65b4
Colin Reinhart <colinbreinhartgmail.com>
2022-04-22 17:46:02 -0600

    print requested text to command line

--- lib/file_writer.rb
+++ lib/file_writer.rb
@@ -1,10 +1,14 @@
 class FileWriter
   attr_reader :in,
               :out
-              
+
   def initialize
-    @in = ARGV[0]
+    @in = File.read(ARGV[0]).chomp
     @out = ARGV[1]
+  end
+
+  def message
+    p "Created 'braille.txt' containing 256 characters"
   end
 
 end

cd4f8da5db61238bb53b89a2c2043a17fbc7e7e7
Colin Reinhart <colinbreinhartgmail.com>
2022-04-22 17:58:49 -0600

    test/dictionary spec

---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,11 @@
+require './lib/dictionary'
+
+describe Dictionary do
+  before :each do
+    @dictionary = Dictionary.new
+  end
+
+  it "exists" do
+    expect(@dictionary).to be_a(Dictionary)
+  end
+end

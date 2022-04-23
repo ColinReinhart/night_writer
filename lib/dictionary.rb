fbd6f13ee5a4828158316dcabcadc4f29ffa2c17
Colin Reinhart <colinbreinhartgmail.com>
2022-04-22 18:15:11 -0600

    to_braille test

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -5,4 +5,35 @@ class Dictionary
     @to_braille = to_braille
   end
 
+  def to_braille
+    {
+      'a' => 'braille a',
+      'b' => 'braille b',
+      'c' => 'braille c',
+      'd' => 'braille d',
+      'e' => 'braille e',
+      'f' => 'braille f',
+      'g' => 'braille g',
+      'h' => 'braille h',
+      'i' => 'braille i',
+      'j' => 'braille j',
+      'k' => 'braille k',
+      'l' => 'braille l',
+      'm' => 'braille m',
+      'n' => 'braille n',
+      'o' => 'braille o',
+      'p' => 'braille p',
+      'q' => 'braille q',
+      'r' => 'braille r',
+      's' => 'braille s',
+      't' => 'braille t',
+      'u' => 'braille u',
+      'v' => 'braille v',
+      'w' => 'braille w',
+      'x' => 'braille x',
+      'y' => 'braille y',
+      'z' => 'braille z',
+    }
+  end
+
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,4 +8,36 @@ describe Dictionary do
   it "exists" do
     expect(@dictionary).to be_a(Dictionary)
   end
+
+  it "has dictionary hash" do
+    expected = {
+      'a' => 'braille a',
+      'b' => 'braille b',
+      'c' => 'braille c',
+      'd' => 'braille d',
+      'e' => 'braille e',
+      'f' => 'braille f',
+      'g' => 'braille g',
+      'h' => 'braille h',
+      'i' => 'braille i',
+      'j' => 'braille j',
+      'k' => 'braille k',
+      'l' => 'braille l',
+      'm' => 'braille m',
+      'n' => 'braille n',
+      'o' => 'braille o',
+      'p' => 'braille p',
+      'q' => 'braille q',
+      'r' => 'braille r',
+      's' => 'braille s',
+      't' => 'braille t',
+      'u' => 'braille u',
+      'v' => 'braille v',
+      'w' => 'braille w',
+      'x' => 'braille x',
+      'y' => 'braille y',
+      'z' => 'braille z',
+    }
+    expect(@dictionary.to_braille).to eq(expected)
+  end
 end
